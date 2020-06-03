7252b5c34385c0fd18779d2cb906f85199a8cca7
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2020-06-03 14:32:17 +0800

    add carousel3

--- template.js
+++ template.js
@@ -54,188 +54,192 @@ export const imageMap = () => {
 
 export const carousel = () => {
     return {
-        "type": "carousel",
-        "contents": [
-            {
-                "type": "bubble",
-                "size": "kilo",
-                "hero": {
-                    "type": "image",
-                    "size": "full",
-                    "aspectRatio": "10:8",
-                    "aspectMode": "cover",
-                    "url": "https://i.imgur.com/geeAuMu.jpg"
-                },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": "門診常見問題",
-                            "wrap": true,
-                            "weight": "bold",
-                            "size": "xl"
-                        },
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "認識更多",
-                                    "wrap": true,
-                                    "weight": "regular",
-                                    "size": "sm",
-                                    "flex": 0,
-                                    "color": "#398CCD",
-                                    "action": {
-                                        "type": "uri",
-                                        "label": "action",
-                                        "uri": "http://pei.usa543.com/LineHome/qa.html#1"
+        "type": "flex",
+        "altText": "carousel flex",
+        "contents": {
+            "type": "carousel",
+            "contents": [
+                {
+                    "type": "bubble",
+                    "size": "kilo",
+                    "hero": {
+                        "type": "image",
+                        "size": "full",
+                        "aspectRatio": "10:8",
+                        "aspectMode": "cover",
+                        "url": "https://i.imgur.com/geeAuMu.jpg"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "門診常見問題",
+                                "wrap": true,
+                                "weight": "bold",
+                                "size": "xl"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "text": "認識更多",
+                                        "wrap": true,
+                                        "weight": "regular",
+                                        "size": "sm",
+                                        "flex": 0,
+                                        "color": "#398CCD",
+                                        "action": {
+                                            "type": "uri",
+                                            "label": "action",
+                                            "uri": "http://pei.usa543.com/LineHome/qa.html#1"
+                                        }
                                     }
-                                }
-                            ]
-                        }
-                    ]
-                }
-            },
-            {
-                "type": "bubble",
-                "size": "kilo",
-                "hero": {
-                    "type": "image",
-                    "size": "full",
-                    "aspectRatio": "10:8",
-                    "aspectMode": "cover",
-                    "url": "https://imgur.com/zni2MFO.jpg"
+                                ]
+                            }
+                        ]
+                    }
                 },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": "產前優生保健",
-                            "wrap": true,
-                            "weight": "bold",
-                            "size": "xl"
-                        },
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "認識更多",
-                                    "wrap": true,
-                                    "weight": "regular",
-                                    "size": "sm",
-                                    "flex": 0,
-                                    "color": "#398CCD",
-                                    "action": {
-                                        "type": "uri",
-                                        "label": "action",
-                                        "uri": "http://pei.usa543.com/LineHome/qa.html#2"
+                {
+                    "type": "bubble",
+                    "size": "kilo",
+                    "hero": {
+                        "type": "image",
+                        "size": "full",
+                        "aspectRatio": "10:8",
+                        "aspectMode": "cover",
+                        "url": "https://imgur.com/zni2MFO.jpg"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "產前優生保健",
+                                "wrap": true,
+                                "weight": "bold",
+                                "size": "xl"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "text": "認識更多",
+                                        "wrap": true,
+                                        "weight": "regular",
+                                        "size": "sm",
+                                        "flex": 0,
+                                        "color": "#398CCD",
+                                        "action": {
+                                            "type": "uri",
+                                            "label": "action",
+                                            "uri": "http://pei.usa543.com/LineHome/qa.html#2"
+                                        }
                                     }
-                                }
-                            ]
-                        }
-                    ]
-                }
-            },
-            {
-                "type": "bubble",
-                "size": "kilo",
-                "hero": {
-                    "type": "image",
-                    "size": "full",
-                    "aspectRatio": "10:8",
-                    "aspectMode": "cover",
-                    "url": "https://imgur.com/KL6f7a6.jpg"
+                                ]
+                            }
+                        ]
+                    }
                 },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": "基因晶片",
-                            "wrap": true,
-                            "weight": "bold",
-                            "size": "xl"
-                        },
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "認識更多",
-                                    "wrap": true,
-                                    "weight": "regular",
-                                    "size": "sm",
-                                    "flex": 0,
-                                    "color": "#398CCD",
-                                    "action": {
-                                        "type": "uri",
-                                        "label": "action",
-                                        "uri": "http://pei.usa543.com/LineHome/qa.html#3"
+                {
+                    "type": "bubble",
+                    "size": "kilo",
+                    "hero": {
+                        "type": "image",
+                        "size": "full",
+                        "aspectRatio": "10:8",
+                        "aspectMode": "cover",
+                        "url": "https://imgur.com/KL6f7a6.jpg"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "基因晶片",
+                                "wrap": true,
+                                "weight": "bold",
+                                "size": "xl"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "text": "認識更多",
+                                        "wrap": true,
+                                        "weight": "regular",
+                                        "size": "sm",
+                                        "flex": 0,
+                                        "color": "#398CCD",
+                                        "action": {
+                                            "type": "uri",
+                                            "label": "action",
+                                            "uri": "http://pei.usa543.com/LineHome/qa.html#3"
+                                        }
                                     }
-                                }
-                            ]
-                        }
-                    ]
-                }
-            },
-            {
-                "type": "bubble",
-                "size": "kilo",
-                "hero": {
-                    "type": "image",
-                    "size": "full",
-                    "aspectRatio": "10:8",
-                    "aspectMode": "cover",
-                    "url": "https://imgur.com/ZgpBD76.jpg"
+                                ]
+                            }
+                        ]
+                    }
                 },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": "非侵入性染色體檢查",
-                            "wrap": true,
-                            "weight": "bold",
-                            "size": "xl"
-                        },
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "認識更多",
-                                    "wrap": true,
-                                    "weight": "regular",
-                                    "size": "sm",
-                                    "flex": 0,
-                                    "color": "#398CCD",
-                                    "action": {
-                                        "type": "uri",
-                                        "label": "action",
-                                        "uri": "http://pei.usa543.com/LineHome/qa.html#4"
+                {
+                    "type": "bubble",
+                    "size": "kilo",
+                    "hero": {
+                        "type": "image",
+                        "size": "full",
+                        "aspectRatio": "10:8",
+                        "aspectMode": "cover",
+                        "url": "https://imgur.com/ZgpBD76.jpg"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "非侵入性染色體檢查",
+                                "wrap": true,
+                                "weight": "bold",
+                                "size": "xl"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "text": "認識更多",
+                                        "wrap": true,
+                                        "weight": "regular",
+                                        "size": "sm",
+                                        "flex": 0,
+                                        "color": "#398CCD",
+                                        "action": {
+                                            "type": "uri",
+                                            "label": "action",
+                                            "uri": "http://pei.usa543.com/LineHome/qa.html#4"
+                                        }
                                     }
-                                }
-                            ]
-                        }
-                    ]
+                                ]
+                            }
+                        ]
+                    }
                 }
-            }
-        ]
+            ]
+        }
     }
 }
