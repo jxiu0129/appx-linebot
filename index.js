1b2ab90386a15173b8c8f4d4f54eaf8764c828da
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2020-06-22 18:30:17 +0800

    test follow(event) & test getProfile(Client)

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const lineConfig = {
 const client = new Client(lineConfig);
 const app = express();
 
+let userId, userName, userPictureUrl; //getProfile用
+
 app.post('/', middleware(lineConfig), async (req, res) => {
     // Promise
     // .all(req.body.events.map(handleEvent))
@@ -18,9 +20,46 @@ app.post('/', middleware(lineConfig), async (req, res) => {
     // })
     // .catch((err) => console.log(err));
     try {
-        console.log(`req.body: ${req.body}`);
-        console.log(`req.body.events: ${req.body.events}`);
-        console.log(`req.body.destination: ${req.body.destination}`);
+        /*{  req.body長這樣
+            "events": [
+                {
+                    "type": "message",
+                    "replyToken": "20d2195fee994542b3db950b6150d7de",
+                    "source": {
+                        "userId": "U8e5c5d1173327f5c016a99f9faf44037",
+                        "type": "user"
+                    },
+                    "timestamp": 1592813446208,
+                    "mode": "active",
+                    "message": {
+                        "type": "text",
+                        "id": "12188948022060",
+                        "text": "X"
+                    }
+                }
+            ],
+            "destination": "Uc50e84eafc37e51091664f6759c51cc2"
+        }*/
+        client.getProfile(req.body.events[0].source.userId).then((profile) => {
+            userId = profile.userId;
+            userName = profile.displayName;
+            userPictureUrl = profile.pictureUrl;
+            /*{ 實驗getProfile
+                userId: 'U8e5c5d1173327f5c016a99f9faf44037',
+                displayName: '杰修',
+                pictureUrl: 'https://profile.line-scdn.net/0hXbG_Of5tB016Ji82aLR4GkZjCSANCAEFAkFIfAsvUHQDRUUcT0lBLV8hCyhQQhdJERIceQ8jXi4A',
+                statusMessage: '(robber)(dumpling)',
+                language: 'en' 
+            }*/
+        });
+
+        // 實驗getNumberOfMessageDeliveries(date: string):
+        client.getNumberOfFollowers('20200622').then( (res)=> {
+            console.log(`getNumberOfFollowers: ${JSON.stringify(res)}`);
+        }).catch((err) => {
+            console.log(err);
+        });
+
         let result = await req.body.events.map(handleEvent);
         res.json(result);
     }
@@ -29,18 +68,19 @@ app.post('/', middleware(lineConfig), async (req, res) => {
     }
 });
 
-const handleEvent = ({type, message, replyToken}) => {
-    switch (type) {
-    case 'join': //加入群組
+const handleEvent = (event) => {
+    switch (event.type) {
+    case 'join': //加入別人的群組
     case 'follow': //追蹤
-        return client.replyMessage(replyToken, {
+        console.log(JSON.stringify(event));
+        return client.replyMessage(event.replyToken, {
         type: 'text',
-        text: `感謝您將本帳號加入好友(happy)\n若不想接收提醒，可以點選本畫面右上方的選單圖示，然後關閉「提醒」的設定喔(ok)?`
+        text: `我知道你是誰喔，你的id是${event.source.userId}\n\n你在${Date(event.timestamp)}加我為好友`
         });   
     case 'message': //傳訊息給機器人
-        switch (message.type) {
+        switch (event.message.type) {
         case 'text':
-            textHandler(replyToken, message.text);
+            textHandler(event.replyToken, event.message.text);
             break;
         case 'sticker':
             // do sth with sticker
@@ -57,12 +97,15 @@ const textHandler = (replyToken, inputText) => {
                 resText = '你好啊';
                 break;
             case 'test':
-                resText = '測試';
+                resText = `測試`;
                 break
             case 'Q&A':
                 return client.replyMessage(replyToken, imageMap());
             case 'q&a':
                 return client.replyMessage(replyToken, carousel());
+            case '我是誰':
+                resText = `你的id為: ${userId}\n你的名字為: ${userName}\n你的照片為: ${userPictureUrl}`;
+                break;
             default:
                 resText = '請親臨院所';
         }
